79758712df24a945f9b4fc8976d2f4109b78cfb6
RickyPWebDev <100093181+RickyPWebDev@users.noreply.github.com>
2024-04-22 16:28:48 +0100

    Update scrollefect.js

--- script/scrollefect.js
+++ script/scrollefect.js
@@ -1,7 +1,12 @@
 
 // this is for the scroll effect on the text. 
 
-ScrollReveal().reveal('.rp-and-p', {delay: 150});
-ScrollReveal().reveal('.right-text', {delay: 150});
-ScrollReveal().reveal('.services', {delay: 150});
+ScrollReveal().reveal('.why', {delay: 300});
+ScrollReveal().reveal('.reason-1', {delay: 450});
+ScrollReveal().reveal('.reason-2', {delay: 500});
+ScrollReveal().reveal('.reason-3', {delay: 550});
 
+// scrollReveal().reveal('.service-scroll-1', {delay: 300});
+// scrollReveal().reveal('.service-scroll-2', {delay: 300});
+// scrollReveal().reveal('.service-scroll-3', {delay: 300});
+// scrollReveal().reveal('.service-scroll-4', {delay: 300});
